17efaa52aac4ad486f01c7a630d1f6644a4a2469
Haoyang Chen <70962093+Haoyang-Chen@users.noreply.github.com>
2023-10-16 15:24:01 +0800

    AtLeastOperator

---
+++ src/main/java/com/rapidminer/extension/ecg_xai/operator/AtLeastOperator.java
@@ -0,0 +1,13 @@
+package com.rapidminer.extension.ecg_xai.operator;
+
+import com.rapidminer.operator.Operator;
+import com.rapidminer.operator.OperatorDescription;
+import com.rapidminer.tools.OperatorService;
+
+public class AtLeastOperator extends Operator {
+
+    public AtLeastOperator(OperatorDescription description) {
+        super(description);
+    }
+}
+
