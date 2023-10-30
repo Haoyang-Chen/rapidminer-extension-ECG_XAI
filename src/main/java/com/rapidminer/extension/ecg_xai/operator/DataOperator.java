1bb89040261986e214c1a22e47680ece1127576b
Haoyang Chen <70962093+Haoyang-Chen@users.noreply.github.com>
2023-10-30 13:14:09 +0800

    data retrieve operator

---
+++ src/main/java/com/rapidminer/extension/ecg_xai/operator/DataOperator.java
@@ -0,0 +1,12 @@
+package com.rapidminer.extension.ecg_xai.operator;
+
+import com.rapidminer.operator.Operator;
+import com.rapidminer.operator.OperatorDescription;
+import com.rapidminer.tools.OperatorService;
+
+public class DataOperator extends Operator {
+    
+    public DataOperator(OperatorDescription description) {
+        super(description);
+    }
+}
